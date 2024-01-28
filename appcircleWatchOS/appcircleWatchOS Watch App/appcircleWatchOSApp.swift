ed92e8048184c2fc331e0a866166f95e5995eb89
boztopuz <boztopuz@gmail.com>
2024-01-28 20:09:49 +0300

    First Commit

---
+++ appcircleWatchOS/appcircleWatchOS Watch App/appcircleWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  appcircleWatchOSApp.swift
+//  appcircleWatchOS Watch App
+//
+//  Created by Burak Öztopuz on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct appcircleWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
